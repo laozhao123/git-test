84a07b8446ebe2b72f5aa61113101e55e0d1b238
admin <zhansan@cast.cn>
2020-01-19 18:32:55 +0800

    2222

--- first-git/src/com/itheima/FirstClass.java
+++ first-git/src/com/itheima/FirstClass.java
@@ -4,4 +4,10 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("123213");
     }
+
+    public void test(){
+
+
+        System.out.println(1231);
+    }
 }
